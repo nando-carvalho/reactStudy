4db3cbffa4053f6b31513c917c7c5e64afbf5ff0
Fernando Carvalho <fernando@elosistemas.inf.br>
2019-11-06 15:06:24 -0300

    ReactJs introduction - projeto rocketseat

---
+++ huntweb/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+
+//class Header extends Component {
+// render() {
+//  return <h1></h1>
+// }
+//}
+//stateless component
+// a inicialização abaixo é o mesmo que se eu fizesse o treixo no comentario
+const Header = () => <header id="main-header">JSHunt</header>;
+
+export default Header;
